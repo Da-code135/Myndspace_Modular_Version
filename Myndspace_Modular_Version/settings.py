b81ab89198fabe09065e9cae5a0075215a0748d0
Kevin Lule <johnlule45@gmail.com>
2025-02-19 03:33:52 +0300

    settings.py update

--- Myndspace_Modular_Version/settings.py
+++ Myndspace_Modular_Version/settings.py
@@ -9,8 +9,8 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Security settings
-SECRET_KEY = os.getenv('SECRET_KEY', '6b431a78-14a4-4e72-8f18-264154c92bd2')
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
 
 # Application definition
