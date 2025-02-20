5cb3cc9fdc332167984a1d7c08a913cef666d8dc
Kevin Lule <johnlule45@gmail.com>
2025-02-20 17:34:58 +0300

    fix2

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Run migrations (if needed)
-python manage.py migrate
-
-# Collect static files
-python manage.py collectstatic --noinput
