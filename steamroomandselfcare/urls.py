da77cb55c541d76d1b2d0f2b28f99e60d9d4b8da
Kevin Lule <johnlule45@gmail.com>
2025-02-20 15:23:16 +0300

    steam room and self care

--- steamroomandselfcare/urls.py
+++ steamroomandselfcare/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('breathing/', views.breathing_exercise, name='breathing_exercise'),
     path('journaling/', views.journaling_prompt, name='journaling_prompt'),
-    path('thought-logs/', views.thought_log, name='thought_log'),
+    path('thought-logs/', views.thought_log, name='thought_logs'),
     path('meditation/', views.meditation, name='meditation'),
 ]
